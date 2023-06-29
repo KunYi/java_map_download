979bb5d37cd80cfd8cd7817f9ae1f98967171a89
Hu <632582183@qq.com>
2023-06-29 22:20:35 +0800

    补充

---
+++ Code/src/main/java/com/jmd/model/setting/AddedLayerSetting.java
@@ -0,0 +1,19 @@
+package com.jmd.model.setting;
+
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Data
+public class AddedLayerSetting implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = -7228487492627563507L;
+
+    private String name;
+    private String url;
+    private String type;
+    private Boolean proxy;
+
+}
