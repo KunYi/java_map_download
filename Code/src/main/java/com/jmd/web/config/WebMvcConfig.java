eaa9e577258d39c9bbfad749f5036ca8760fa59d
KunYi Chen <kunyi.chen@gmail.com>
2024-10-09 17:41:02 +0800

    fixed Tomcat cannot open map issues
    when run on Linux will get 'Page not found' message
    for /*.js & /*.css files

--- Code/src/main/java/com/jmd/web/config/WebMvcConfig.java
+++ Code/src/main/java/com/jmd/web/config/WebMvcConfig.java
@@ -6,6 +6,8 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.jmd.common.StaticVar;
+
 @EnableWebMvc
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
@@ -13,6 +15,14 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/web/**").addResourceLocations("classpath:/web/");
+        if (StaticVar.IS_LINUX) {
+            registry.addResourceHandler("/chunk-**").addResourceLocations("classpath:/web/");
+            registry.addResourceHandler("/scripts-**").addResourceLocations("classpath:/web/");
+            registry.addResourceHandler("/main-**").addResourceLocations("classpath:/web/");
+            registry.addResourceHandler("/styles-**").addResourceLocations("classpath:/web/");
+            registry.addResourceHandler("/polyfills-**").addResourceLocations("classpath:/web/");
+            registry.addResourceHandler("/favicon.ico").addResourceLocations("classpath:/web/");
+        }
     }
 
     @Override
